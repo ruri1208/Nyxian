2ae6d1e1a3a3a3e2d517a85d33b80d0a7f5cd358
Ruri <888mfvjygb@privaterelay.appleid.com>
2026-08-26 22:43:53 +0800

    Replace EmptyView with UIViewController in Settings

--- Nyxian/UI/Settings/SettingsViewController.swift
+++ Nyxian/UI/Settings/SettingsViewController.swift
@@ -116,7 +116,7 @@ class SettingsViewController: UIThemedTableViewController {
                 if NXApplicationState.extensionLessMode {
                     return CreditsViewController(style: .insetGrouped)
                 } else {
-                    return EmptyView()
+                    return UIViewController()
                 }
             case 3:
                 return CustomizationViewController(style: .insetGrouped)
